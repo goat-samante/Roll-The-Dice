5d26560f13c2a731587ce2c267ba467b51acc92c
Samuel Amante <amantsa76@gmail.com>
2024-10-21 09:41:53 -0500

    Initial Commit

---
+++ Roll The Dice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roll The Dice
+//
+//  Created by Samuel Amante on 10/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roll The Dice/Roll_The_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_The_DiceApp.swift
+//  Roll The Dice
+//
+//  Created by Samuel Amante on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Roll_The_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
